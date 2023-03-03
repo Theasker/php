aa54e2e8ec692b253efa2bb9ed9e358dd21ad7c2
Mauricio Segura <theasker@gmail.com>
2023-03-03 09:55:19 +0000

    sendMedia en TelegramBot

--- TelegramBot.php
+++ TelegramBot.php
@@ -112,7 +112,70 @@ class TelegramBot {
         return $result;
     }
 
-    function sendMedia($token, $urlphoto, $type, $chatid = '-797062014', $caption = "") {
+    function sendMedia($token, $chat_id, $filename, $type = '', $caption = '') {
+        $api_url = $this->url.$token."/";
+        
+        if (strpos($type, 'image') !== false) {
+          $method = 'sendPhoto';
+          $field = 'photo';
+        } elseif (strpos($type, 'audio') !== false) {
+          $method = 'sendAudio';
+          $field = 'audio';
+        } elseif (strpos($type, 'video') !== false) {
+          $method = 'sendVideo';
+          $field = 'video';
+        } else {
+          $method = 'sendDocument';
+          $field = 'document';
+        }
+        
+        $file = curl_file_create($filename, $type);
+        $data = array(
+          'chat_id' => $chat_id,
+          'caption' => $caption,
+          $field => $file
+        );
+      
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $api_url.$method);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:multipart/form-data'));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+         
+    // Envía foto de una URL
+    function sendPhoto($token, $chatid, $urlphoto, $caption = ""){
+        $file = $this->saveFile($urlphoto);
+        $photo = new CURLFile(realpath($file));
+        // echo "$chatid\n$urlphoto\n$caption";
+        $data = array(
+            'chat_id' => $chatid,
+            'photo' => $photo,
+            'caption' => $caption,
+            'parse_mode' => 'html',
+        );
+        $options = array(
+            CURLOPT_URL => "https://api.telegram.org/bot$token/sendPhoto",
+            CURLOPT_POST => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => $data
+        );
+        $curl = curl_init();
+        curl_setopt_array($curl, $options);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $result = json_decode($response, true);
+        $this->deleteFile($file);
+        return $response;
+    }
+
+    /*
+    function sendMedia($token, $urlphoto, $type = '', $chatid = '-797062014', $caption = "") {
         try {
             $name = $this->saveFile($urlphoto);
             $file = fopen("./tmp/$name", 'rb') or die("Unable to open file!");
@@ -151,7 +214,56 @@ class TelegramBot {
             echo "Error: " . $e->getMessage();
         }
     }
+    */
 
+    /*
+    function send_media($token, $chat_id, $filename, $type = '', $caption = '') {
+        // intenta abrir el archivo para verificar si existe
+        try {
+            $file = fopen($filename, 'rb');
+            $method = '';
+            $data = array(
+                'chat_id' => $chat_id,
+                'caption' => $caption
+            );
+            if ($type == "photo") {
+                $method = "sendPhoto";
+                $files = array('photo' => $file);
+            } elseif ($type == "audio") {
+                $method = "sendAudio";
+                $files = array('audio' => $file);
+            } elseif ($type == "video") {
+                $method = "sendVideo";
+                $files = array('video' => $file);
+            } else {
+                $data = array(
+                    'chat_id' => $chat_id,
+                    'caption' => $caption,
+                    'disable_content_type_detection' => False
+                );
+                $method = "sendDocument";
+                $files = array('document' => $file);
+            }
+            echo "method: $method\n";
+            echo "url: $this->url\n";
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $this->url . $method);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $files);
+            $response = curl_exec($curl);
+            curl_close($curl);
+            //return json_decode($response, true);
+            return $response;
+        } catch (Exception $e) {
+            echo 'Excepción capturada: ', $e->getMessage(), "\n";
+        }
+    }
+    */
+
+    // Graba un fichero de una URL
     private function saveFile($url){
         $file_contents = file_get_contents($url);
         $array_url = explode('/',$url);
@@ -160,37 +272,14 @@ class TelegramBot {
         return "/tmp/$name";
     }
 
+    // borra un fichero del sistema
     private function deleteFile($file){
         if (file_exists($file)) {
             unlink($file);
         }
     }
-
-    function sendPhoto($token, $chatid, $urlphoto, $caption = ""){
-        $file = $this->saveFile($urlphoto);
-        $photo = new CURLFile(realpath($file));
-        // echo "$chatid\n$urlphoto\n$caption";
-        $data = array(
-            'chat_id' => $chatid,
-            'photo' => $photo,
-            'caption' => $caption,
-            'parse_mode' => 'html',
-        );
-        $options = array(
-            CURLOPT_URL => "https://api.telegram.org/bot$token/sendPhoto",
-            CURLOPT_POST => true,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POSTFIELDS => $data
-        );
-        $curl = curl_init();
-        curl_setopt_array($curl, $options);
-        $response = curl_exec($curl);
-        curl_close($curl);
-        $result = json_decode($response, true);
-        $this->deleteFile($file);
-        return $response;
-    }
 }
+
 // $bot = new TelegramBot();
 // $token = "5877626268:AAH20O9i4qYjz_0988SPCvmw0xGgVKy3ZPA";
 
--- theasker/index.php
+++ theasker/index.php
@@ -29,6 +29,7 @@ class TheaskerBot extends TelegramBot{
             <code>!comando <comando></code>: Ejecuta comando en el sistema operativo
             <code>!wol</code>: Inicia el ordenador de casa
             <code>!traducir (es|en) <texto></code>: Traduce un texto de Inglés-español o español-inglés.
+            <code>!clonewars</code>: Nos escribe una frase de Clone Wars aleatoria.
         EOD;
         return $str;
     }
@@ -63,11 +64,125 @@ class TheaskerBot extends TelegramBot{
             }else {
                 $out = "Idioma seleccionado no es correcto";
             }
-
-        }else $out = "Comando \"<code>$message</code>\" => No hago nada";
+        }else if (str_starts_with($message, '!clonewars')) {
+            $out = $this->cloneWarsPhrase();
+        }
+        else $out = "Comando \"<code>$message</code>\" => No hago nada";
         return $out;
     }
 
+    public function cloneWarsPhrase() {
+        $lines = file($this->ini_array['CLONEWARSFILE']); // Lee el archivo y guarda cada línea en un array
+        $phrase = explode(' - ', $lines[rand(0,count($lines))]);
+        //var_dump("$phrase[0] - $phrase[1]:\n $phrase[2]");
+        $text = str_replace("\n","",$phrase[2]);
+        return "<b><i>\"$text\"</i></b>";
+    }
+
+    public function youtubeCheckChannels() {
+        // Replace with your own API key
+        $api_key = $this->ini_array['YOUTUBEAPIKEY'];
+        
+        foreach ($this->ini_array['YOUTUBECHANNELS'] as $key => $value) {
+            // Crea la URL de la API de YouTube Data para buscar el canal
+            
+            // Channel id from youtube video url
+            //$array_url = explode("=",$value);
+            //$video_id = $array_url[1];
+            //$api_url = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id={$video_id}&key={$api_key}";
+            ///////////////////////////////////
+
+            // Channel id from youtube custom channel url
+            $array_url = explode("=",$value);
+            $channel_name = $array_url[count($array_url)-1];
+            $api_url = "https://www.googleapis.com/youtube/v3/search?part=id%2Csnippet&q=$channel_name&type=channel&key={$api_key}";
+            
+
+            // Haz una llamada a la API y obtén la respuesta JSON           
+            $response = file_get_contents($api_url);           
+            $json = json_decode($response);
+                 
+            // Extrae el ID del canal de la respuesta JSON
+            $channel_id = $json->items[0]->snippet->channelId;
+
+            // Call the YouTube Data API to get the latest video from the channel
+            $api_url = "https://www.googleapis.com/youtube/v3/search?key={$api_key}&channelId={$channel_id}&part=snippet,id&order=date&maxResults=1";
+            $response = file_get_contents($api_url);
+            $json = json_decode($response);
+
+            // Extract the video ID and title from the response
+            $video_id = $json->items[0]->id->videoId;
+            $video_title = $json->items[0]->snippet->title;
+
+            // Check if the video ID has changed since the last check
+            $last_video_id = file_get_contents('./last_video_id.txt');
+            if ($video_id != $last_video_id) {
+                // Download the video using the ID and title
+                $video_url = "https://www.youtube.com/watch?v={$video_id}";
+                $video_path = "./videos/{$video_title}.mp4";
+                file_put_contents($video_path, file_get_contents($video_url));
+                
+                // Save the new video ID for the next check
+                file_put_contents('./last_video_id.txt', $video_id);
+                echo 'New video downloaded: ' . $video_title;
+            } else {
+                echo 'No new videos found';
+            }
+            
+
+        }
+        
+
+
+
+    }
+
+    public function getYoutubeVideo($youtubeURL) {
+        // Load and initialize downloader class 
+        include_once 'YouTubeDownloader.class.php'; 
+        $handler = new YouTubeDownloader(); 
+        
+        // Youtube video url 
+        //$youtubeURL = 'https://www.youtube.com/watch?v=f7wcKoEbUSA'; 
+        
+        // Check whether the url is valid 
+        if(!empty($youtubeURL) && !filter_var($youtubeURL, FILTER_VALIDATE_URL) === false){ 
+            // Get the downloader object 
+            $downloader = $handler->getDownloader($youtubeURL); 
+            
+            // Set the url 
+            $downloader->setUrl($youtubeURL); 
+            
+            // Validate the youtube video url 
+            if($downloader->hasVideo()){ 
+                // Get the video download link info 
+                $videoDownloadLink = $downloader->getVideoDownloadLink(); 
+                
+                $videoTitle = $videoDownloadLink[0]['title']; 
+                $videoQuality = $videoDownloadLink[0]['qualityLabel']; 
+                $videoFormat = $videoDownloadLink[0]['format']; 
+                $videoFileName = strtolower(str_replace(' ', '_', $videoTitle)).'.'.$videoFormat; 
+                $downloadURL = $videoDownloadLink[0]['url']; 
+                $fileName = preg_replace('/[^A-Za-z0-9.\_\-]/', '', basename($videoFileName)); 
+                
+                if(!empty($downloadURL)){ 
+                    // Define header for force download 
+                    header("Cache-Control: public"); 
+                    header("Content-Description: File Transfer"); 
+                    header("Content-Disposition: attachment; filename=$fileName"); 
+                    header("Content-Type: application/zip"); 
+                    header("Content-Transfer-Encoding: binary"); 
+                    
+                    // Read the file 
+                    readfile($downloadURL); 
+                } 
+            }else{ 
+                echo "The video is not found, please check YouTube URL."; 
+            } 
+        }else{ 
+            echo "Please provide valid YouTube URL."; 
+        } 
+    }
 }
 
 function pipedream($update) {
@@ -90,10 +205,14 @@ function pipedream($update) {
 
 function theasker() {
     $bot = new TheaskerBot();
-    $out = $bot->dispatcher("!traducir en hola que tal estas?", "-797062014");
-    echo $out;
+    // $out = $bot->dispatcher("!clonewars", "-797062014");
+    // $bot->youtubeCheckChannels();
+    //$bot->sendText($bot->ini_array['TOKEN'], $out, "-797062014");
     
-    $bot->sendText($bot->ini_array['TOKEN'], $out, "-797062014");    
+    $path_file = 'video.mp4';
+    
+    // sendMedia($token, $chat_id, $filename, $type, $caption = '')
+    echo $bot->sendMedia($bot->ini_array['TOKEN'], "-797062014", $path_file, 'video');
 }
 
 theasker();
